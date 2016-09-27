95a98aa06ac26c48d09907323aeb3fb253b9a2ec
tbrou <tbrou@tbrou-PC7.mshome.net>
2016-09-27 20:27:50 +0200

    * spring + junit + custom log framework

---
+++ maven-test/src/main/java/fr/tbr/iam/User.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package fr.tbr.iam;
+
+/**
+ * @author tbrou
+ *
+ */
+public class User {
+	
+	private String firstName;
+	private String lastName;
+	private String login;
+	
+	/**
+	 * @return the login
+	 */
+	public final String getLogin() {
+		return login;
+	}
+	/**
+	 * @param login the login to set
+	 */
+	public final void setLogin(String login) {
+		this.login = login;
+	}
+	/**
+	 * @return the firstName
+	 */
+	public final String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public final void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+}
---
+++ maven-test/src/main/java/fr/tbr/iam/log/impl/IAMLogManager.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package fr.tbr.iam.log.impl;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+
+import fr.tbr.iam.log.IAMLogger;
+
+/**
+ * @author tbrou
+ *
+ */
+public class IAMLogManager {
+	
+	private static final Logger logger = LogManager.getLogger(IAMLogManager.class);
+	
+	
+	/**
+	 * 
+	 * @param clazz
+	 * @return
+	 */
+	public static IAMLogger getIAMLogger(Class<?> clazz){
+		LoggerContext context = (LoggerContext) LogManager.getContext(clazz.getClassLoader(), false);
+		return new IAMLoggerLog4j2Impl(context, clazz.getName(), logger.getMessageFactory());
+	}
+
+}
