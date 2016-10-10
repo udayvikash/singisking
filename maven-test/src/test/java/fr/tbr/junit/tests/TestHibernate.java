677825e614ae51f405b4024fb97484cd5c83ad31
tbrou <tbrou@tbrou-PC7.mshome.net>
2016-10-10 19:39:17 +0200

    * hibernate integration

--- maven-test/src/test/java/fr/tbr/junit/tests/TestHibernate.java
+++ maven-test/src/test/java/fr/tbr/junit/tests/TestHibernate.java
@@ -3,32 +3,42 @@
  */
 package fr.tbr.junit.tests;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import fr.tbr.iam.User;
-
 /**
  * @author tbrou
  *
  */
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="/applicationContext.xml")
-public class TestSpring {
+public class TestHibernate {
 	
 	@Autowired
-	User user;
+	DataSource ds;
+	
 	
 	@Test
-	public void testUserIsNotNull(){
-		System.out.println(user.getFirstName());
+	public void testDataSource() throws SQLException{
+		Assert.assertNotNull(ds);
+		Connection connection = ds.getConnection();
+		connection.close();
 	}
 	
 	
 	
+//	@Test
+//	public void testHibernate(){
+//		
+//	}
 
 }
